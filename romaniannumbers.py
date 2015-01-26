b58a95241b12528119ade13eddf5132832854418
Jeyloh <jlh.94@hotmail.com>
2015-01-26 09:39:58 +0100

    Janis' romertall oppg

---
+++ romaniannumbers.py
@@ -0,0 +1,9 @@
+# Tar INPUT i form av desimalall
+# Behandler/Konverterer
+# OUTPUT i form av romertall
+
+# Sjekk om INPUT er et tall
+# Hvis INPUT er et desimaltall
+	# Gjør dette
+# Visst ikke
+	# Gjør dette
