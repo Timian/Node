5658eb46108da3fa50a21afb36dfb264ce305962
Jeyloh <jlh.94@hotmail.com>
2015-01-26 10:00:54 +0100

    lab2 mappesystem

---
+++ romanianNumbers/is105roman.py
@@ -0,0 +1,26 @@
+# Oppgave
+# Designe en module i Python som kan overføre fra det desimale til det romerske
+# tallsystemet og omvendt.
+# Implementere addisjon av to romerske tall (uten å bruke overgangen til
+# desimaltall).
+# For å få ekstra poeng, implementer også subtraksjon og multiplikasjon.
+# Innlevering skal inneholde en module is105roman.py og en egen fil hvor man
+# demonstrerer hvordan funksjoner i modulen fungerer (testing). I tillegg levere dere
+# en tekst-fil hvor dere svarer på følgende:
+# (1) Hva er grunnleggende forskjeller på det desimale og det romerske
+# tallsystemet? (forklar fra designperspektivet)
+# (2) Hva er grunnleggende systeminformasjon for systemet som testene er blitt
+# utført på (operativssystemet, distribusjon, informasjon om CPU type)? Se
+# under for forslag hvordan man finner slik informasjon.
+
+
+
+# Tar INPUT i form av desimalall
+# Behandler/Konverterer
+# OUTPUT i form av romertall
+
+# Sjekk om INPUT er et tall
+# Hvis INPUT er et desimaltall
+	# Gjør dette
+# Visst ikke
+	# Gjør dette
