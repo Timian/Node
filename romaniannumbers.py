fb5f1fbaa1372e2c2af7cf4866aebce32b445880
Jeyloh <jlh.94@hotmail.com>
2015-01-26 10:05:13 +0100

    fjernet overflødig fil

--- romaniannumbers.py
+++
@@ -1,9 +0,0 @@
-# Tar INPUT i form av desimalall
-# Behandler/Konverterer
-# OUTPUT i form av romertall
-
-# Sjekk om INPUT er et tall
-# Hvis INPUT er et desimaltall
-	# Gjør dette
-# Visst ikke
-	# Gjør dette
