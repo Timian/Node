03079663a1ca501284a053e568449f96b13872f2
Kurrio <danielconnery1@hotmail.com>
2015-02-02 14:09:28 +0100

    fikset

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -182,7 +182,6 @@ transferHex(string)
 ## Kravspesifikasjon for denne funksjonen er den samme som for ascii8Bin funksjonen
 ## Bruker skriver inn en bokstav. Unicode er ok.
 print "\nSkriv en bokstav, gjerne Æ Ø eller Å. Den kan være stor eller liten:" 
-char = raw_input("> ")
 character = unicode(raw_input(), 'utf8')
 
 def unicodeBin(character):
